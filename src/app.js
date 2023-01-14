843493252f971043177dbe4949d1ae950180c097
jknox927 <joshua.knox927@gmail.com>
2023-01-14 02:59:11 +0000

    Init commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const update = () => {
+
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const suits = [
+      "<span class='red'>&hearts;</span>", 
+      "<span class='black'>&spades;</span>", 
+      "<span class='black'>&clubs;</span>", 
+      "<span class='red'>&diam;</span>"
+    ];
+  
+    let suit = suits[Math.floor(Math.random() * suits.length)];
+    document.querySelector(".card-header").innerHTML = suit;
+    document.querySelector(".card-footer").innerHTML = suit;
+    document.querySelector(".card-value").innerHTML = values[Math.floor(Math.random() * values.length)];
+
+  }
+
+  update();
+
+  document.querySelector(".btn").onclick = update;
+  console.log(update);
+
+  
+
+}  
+
